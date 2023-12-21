8feedbce5781d9d25368ec68caeb0fbbe6807423
Mohit Pathak <itspmohit@gmail.com>
2023-12-21 19:48:07 +0530

    fix: fix typo on the api uri

--- src/Helpers/FetchCityData.js
+++ src/Helpers/FetchCityData.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const FetchCityData = async (cityName) => {
   try {
-    const uri = `http://api.openweathermap.org/geo/1.0/direct?q=${cityName}&limit=1&appid=${
+    const uri = `https://api.openweathermap.org/geo/1.0/direct?q=${cityName}&limit=1&appid=${
       import.meta.env.VITE_OPENWEATHER_API_KEY
     }`;
     const response = await axios.get(uri);
